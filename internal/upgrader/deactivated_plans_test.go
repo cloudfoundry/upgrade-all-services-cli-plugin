82e2a43a42a4cee6bb6dead9ae8d6d9d59f6342b
George Blue <george.blue@broadcom.com>
2025-09-29 11:31:07 +0100

    test: refactor to match implementation file layout
    The file layout for the implementation of the upgrader package
    has changed. It makes sense to move the unit tests so that they
    follow the same pattern, so that it's easy to locate the unit
    tests for any particular action.

---
+++ internal/upgrader/deactivated_plans_test.go
@@ -0,0 +1,94 @@
+package upgrader_test
+
+import (
+	"upgrade-all-services-cli-plugin/internal/ccapi"
+	"upgrade-all-services-cli-plugin/internal/config"
+	"upgrade-all-services-cli-plugin/internal/upgrader"
+	"upgrade-all-services-cli-plugin/internal/upgrader/upgraderfakes"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("--check-deactivated-plans", func() {
+	const (
+		fakeBrokerName   = "fake-broker-name"
+		fakePlanGUID     = "fake-plan-guid"
+		fakeInstanceGUID = "fake-instance-guid"
+	)
+
+	var (
+		fakeCFClient *upgraderfakes.FakeCFClient
+		fakeLogger   *upgraderfakes.FakeLogger
+	)
+
+	BeforeEach(func() {
+		fakeCFClient = &upgraderfakes.FakeCFClient{}
+		fakeLogger = &upgraderfakes.FakeLogger{}
+	})
+
+	When("there are no deactivated plans", func() {
+		BeforeEach(func() {
+			fakeCFClient.GetServicePlansReturns([]ccapi.ServicePlan{
+				{GUID: fakePlanGUID, Available: true, MaintenanceInfoVersion: "1.2.3"},
+			}, nil)
+			fakeCFClient.GetServiceInstancesForServicePlansReturns([]ccapi.ServiceInstance{
+				{
+					GUID:                              fakeInstanceGUID,
+					UpgradeAvailable:                  false,
+					ServicePlanGUID:                   fakePlanGUID,
+					LastOperationType:                 "create",
+					LastOperationState:                "succeeded",
+					MaintenanceInfoVersion:            "1.2.3",
+					ServicePlanMaintenanceInfoVersion: "1.2.3",
+					ServicePlanDeactivated:            false,
+				},
+			}, nil)
+		})
+
+		It("succeeds", func() {
+			output := captureStdout(func() {
+				err := upgrader.Upgrade(fakeCFClient, fakeLogger, upgrader.UpgradeConfig{
+					BrokerName: fakeBrokerName,
+					Action:     config.CheckDeactivatedPlansAction,
+				})
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			Expect(output).NotTo(ContainSubstring(fakeInstanceGUID))
+		})
+	})
+
+	When("there are deactivated plans", func() {
+		BeforeEach(func() {
+			fakeCFClient.GetServicePlansReturns([]ccapi.ServicePlan{
+				{GUID: fakePlanGUID, Available: false, MaintenanceInfoVersion: "1.2.3"},
+			}, nil)
+			fakeCFClient.GetServiceInstancesForServicePlansReturns([]ccapi.ServiceInstance{
+				{
+					GUID:                              fakeInstanceGUID,
+					UpgradeAvailable:                  false,
+					ServicePlanGUID:                   fakePlanGUID,
+					LastOperationType:                 "create",
+					LastOperationState:                "succeeded",
+					MaintenanceInfoVersion:            "1.2.3",
+					ServicePlanMaintenanceInfoVersion: "1.2.3",
+					ServicePlanDeactivated:            true,
+				},
+			}, nil)
+		})
+
+		It("returns an error", func() {
+			output := captureStdout(func() {
+				err := upgrader.Upgrade(fakeCFClient, fakeLogger, upgrader.UpgradeConfig{
+					BrokerName: fakeBrokerName,
+					Action:     config.CheckDeactivatedPlansAction,
+				})
+				Expect(err).To(MatchError("discovered deactivated plans associated with instances"))
+				Expect(err).To(BeAssignableToTypeOf(upgrader.InstanceError{}))
+			})
+
+			Expect(output).To(ContainSubstring(fakeInstanceGUID))
+		})
+	})
+})
